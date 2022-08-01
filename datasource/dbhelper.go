39a9ef8c4d128247b914fd688fa10db217c3252a
xuhuan <wit.huan@outlook.com>
2022-08-01 16:07:30 +0800

    [4-7:4-8], 创建数据库及redis实例

---
+++ datasource/dbhelper.go
@@ -0,0 +1,45 @@
+package datasource
+
+import (
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/go-xorm/xorm"
+	"log"
+	"lottery/conf"
+	"sync"
+)
+
+var masterInstance *xorm.Engine
+
+var dbLock sync.Mutex
+
+func InstanceDbMaster() *xorm.Engine {
+	if masterInstance != nil {
+		return masterInstance
+	}
+	dbLock.Lock()
+	defer dbLock.Unlock()
+	if masterInstance != nil {
+		return masterInstance
+	}
+
+	return NewDbMaster()
+}
+
+func NewDbMaster() *xorm.Engine {
+	sourcename := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8",
+		conf.DbMaster.User,
+		conf.DbMaster.Pwd,
+		conf.DbMaster.Host,
+		conf.DbMaster.Port,
+		conf.DbMaster.Database)
+	instance, err := xorm.NewEngine(conf.DriverName, sourcename)
+	if err != nil {
+		log.Fatal("dbhelper.NewDbMaster NewEngine error ", err)
+		return nil
+	}
+	instance.ShowSQL(true)
+
+	masterInstance = instance
+	return instance
+}
---
+++ datasource/rdshelper.go
@@ -0,0 +1,87 @@
+package datasource
+
+import (
+	"fmt"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"lottery/conf"
+	"sync"
+	"time"
+)
+
+var rdsLock sync.Mutex
+var cacheInstance *RedisConn
+
+type RedisConn struct {
+	pool      *redis.Pool
+	showDebug bool
+}
+
+func (rds *RedisConn) Do(commandName string, args ...interface{}) (reply interface{}, err error) {
+	conn := rds.pool.Get()
+	defer conn.Close()
+
+	t1 := time.Now().UnixNano()
+	reply, err = conn.Do(commandName, args...)
+	if err != nil {
+		e := conn.Err()
+		if e != nil {
+			log.Println("rdshelper.Do", err, e)
+		}
+	}
+	t2 := time.Now().UnixNano()
+	if rds.showDebug {
+		fmt.Printf("[redis] [info] [%dus]cmd=%s, err=%s, args=%v, reply=%s\n",
+			(t2-t1)/1000, commandName, err, args, reply)
+	}
+	return reply, err
+}
+
+func (rds *RedisConn) ShowDebug(b bool) {
+	rds.showDebug = b
+}
+
+func InstanceCache() *RedisConn {
+	if cacheInstance != nil {
+		return cacheInstance
+	}
+	rdsLock.Lock()
+	defer rdsLock.Unlock()
+
+	if cacheInstance != nil {
+		return cacheInstance
+	}
+	return NewCache()
+}
+
+func NewCache() *RedisConn {
+	pool := redis.Pool{
+		Dial: func() (redis.Conn, error) { //初始化连接的代码信息
+			c, err := redis.Dial("tcp", fmt.Sprintf("%s:%d", conf.RdsCache.Host, conf.RdsCache.Port))
+			if err != nil {
+				log.Fatal("rdshelper.NewCache Dial error= ", err)
+				return c, err
+			}
+			return c, nil
+		},
+		TestOnBorrow: func(c redis.Conn, t time.Time) error {
+			if time.Since(t) < time.Minute {
+				return nil
+			}
+			_, err := c.Do("PING")
+			return err
+		},
+		MaxIdle:         10000, //最大空闲链接数
+		MaxActive:       10000, //表示和数据库的最大链接数，0表示没有限制
+		IdleTimeout:     0,     //最大空闲时间
+		Wait:            false,
+		MaxConnLifetime: 0,
+	}
+
+	instance := &RedisConn{
+		pool: &pool,
+	}
+	cacheInstance = instance
+	instance.ShowDebug(true)
+	return instance
+}
